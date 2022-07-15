e7fe262b7a14f86a7f67350635d6525889bfe34c
Lorraine <lorraineconnellyzero@gmail.com>
2022-07-15 14:30:25 +0800

    LinkList.js component now queries database instead of using mock data.

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import Link from './Link';
+import { useQuery, gql } from '@apollo/client';
+
+const FEED_QUERY = gql`
+    {
+        feed {
+            id
+            links {
+                id
+                createdAt
+                url
+                description
+            }
+        }
+    }
+`
+;
 
 const LinkList = () => {
-    const linksToRender = [
-        {
-            id: 'link-id-1',
-            description:
-            'Prisma gives you a powerful database toolkit 😎',
-            url: 'https://prisma.io'
-        },
-        {
-            id: 'link-id-2',
-            description: 'The best GraphQL client',
-            url: 'https://www.apollographql.com/docs/react/'
-        },
-        {
-            id: 'link-id-2',
-            description: 'This data is hard coded! No database access has been implemented yet!',
-            url: ''
-        }
-    ];
-    
+    const { data } = useQuery(FEED_QUERY);
+
     return (
         <div>
-            {linksToRender.map((link) => (
-                <Link key={link.id} link={link} />
-            ))}
+            {data && (
+                <>
+                    {data.feed.links.map((link) => (
+                        <Link key={link.id} link={link} />
+                    ))}
+                </>
+            )}
+            Here!
         </div>
     );
 };

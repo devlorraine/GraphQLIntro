eb45e10ec141f9e75663a62d6f26ab6f8bb8e4da
Lorraine <lorraineconnellyzero@gmail.com>
2022-07-28 15:00:00 +0800

    CreateLink component now updates client cache when creating new link.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FEED_QUERY } from './LinkList';
 import { useMutation, gql } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 
@@ -28,6 +29,20 @@ const CreateLink = () => {
         variables: {
             description: formState.description,
             url: formState.url
+        },
+        update: (cache, { data: { post } }) => {
+            const data = cache.readQuery({
+                query: FEED_QUERY,
+            });
+            
+            cache.writeQuery({
+                query: FEED_QUERY,
+                data: {
+                    feed: {
+                        links: [post, ...data.feed.links]
+                    }
+                },
+            });
         },
         onCompleted: () => navigate('/')
     });
